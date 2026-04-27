40fedb44023e457bc0ab51c733d139fb89b2ae80
catalinapetasny <petasnycatalina@gmail.com>
2026-04-27 10:54:06 -0300

    Perfeccionar Footer; registro e inicio de sesion hecho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './paginas/inicio/inicio';
+import { Favoritos } from './compartidos/favoritos/favoritos';
+import { Contacto } from './paginas/contacto/contacto';
+import { Productos } from './paginas/productos/productos';
+import { Registro } from './auth/registro/registro';
+import { Carrito } from './compartidos/carrito/carrito';
+import { InicioSesion } from './auth/inicio-sesion/inicio-sesion';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'inicio', pathMatch: 'full'},
+    
+    //Acceso a las rutas
+    { path: 'inicio', component: Inicio },
+    { path: 'favoritos', component: Favoritos },
+    { path: 'contacto', component: Contacto },
+    { path: 'productos', component: Productos},
+    { path: 'registro', component: Registro },
+    { path: 'carrito', component: Carrito },
+    { path: 'inicio-sesion', component: InicioSesion },
+
+    { path: '**', redirectTo: 'inicio' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './compartidos/menu/menu';
+import { Footer } from './compartidos/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
